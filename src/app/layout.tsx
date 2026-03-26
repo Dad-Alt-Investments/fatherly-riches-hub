84d95291db748aee54a9e4ead0a36f1e6a21f2de
jnashreal <jared@jareddevalk.com>
2026-03-26 16:02:59 -0500

    feat: manually implement GTM script snippets via user request

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Zilla_Slab, Source_Sans_3 } from "next/font/google";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -32,7 +33,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className={`${zillaSlab.variable} ${sourceSans3.variable} antialiased`}>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PT23N83B');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning className="bg-background text-foreground flex min-h-screen flex-col">
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PT23N83B"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <TooltipProvider>
           <Header />
           <main className="flex-1">
