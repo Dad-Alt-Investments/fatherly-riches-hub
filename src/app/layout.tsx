22515cf64c71cb6e347927c11bf8b1c299271286
jnashreal <jared@jareddevalk.com>
2026-03-27 08:11:49 -0500

    fix: use beforeInteractive Script strategy to bypass App Router head limitations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 import type { Metadata } from "next";
 import { Zilla_Slab, Source_Sans_3 } from "next/font/google";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -33,8 +33,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className={`${zillaSlab.variable} ${sourceSans3.variable} antialiased`}>
-      <GoogleTagManager gtmId="GTM-NC5XR6GW" />
+      <Script
+        id="gtm-script"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NC5XR6GW');
+          `,
+        }}
+      />
       <body suppressHydrationWarning className="bg-background text-foreground flex min-h-screen flex-col">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NC5XR6GW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <TooltipProvider>
           <Header />
           <main className="flex-1">
